88028274a395fac90c550a42571370306a701bec
Ngô Ngọc Hải <ngochai11012004@gmail.com>
2025-05-13 19:39:43 +0700

    Delete app directory

--- app/router.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require('express');
-
-const router = new Router();
-
-router.use('/v1', require('./v1/route'));
-
-router.get('/health-check', async (req, res, next) => res.status(200).send());
-
-module.exports = router;
--- app/v1/route.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require('express');
-
-const router = new Router();
-
-router.use('/products', require('./products/route'));
-router.use('/files', require('./files/route'));
-router.use('/auth', require('./auth/route'));
-
-module.exports = router;
